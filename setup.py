c6212edc6a1acd2aba049a371f80a55c36a73860
jessekrubin <jessekrubin@gmail.com>
2018-07-09 23:50:19 -0700

    herm

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-
 setup(name='pupy',
       version='2.0.1a',
       description='pretty useful python',
@@ -13,14 +12,14 @@ setup(name='pupy',
       ],
       keywords='pretty useful python',
       # url='http://github.com/jessekrubin/pupy/',
+      repository='https://upload.pypi.org/legacy/',
       url='https://upload.pypi.org/legacy/',
-      author='jesse r',
+      author='jessekrubin',
       author_email='jessekrubin@gmail.com',
       license='MIT',
       packages=find_packages(),
       install_requires=[
-          'markdown',
-          'tqdm>=4',
+          'tqdm>=4'
       ],
       include_package_data=True,
       zip_safe=False)
