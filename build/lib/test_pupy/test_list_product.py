670e7d43457db54d5d51c4a397f451dfa9a682cd
jessekrubin <jessekrubin@gmail.com>
2018-07-09 23:25:31 -0700

    idk what is happening

---
+++ build/lib/test_pupy/test_list_product.py
@@ -0,0 +1,25 @@
+# coding=utf-8
+from __future__ import division, print_function
+import pyximport; pyximport.install()
+from time import time
+from pupy.decorations import tictoc
+
+l = [i for i in range(1, 100000)]
+#
+# def _bib_test():
+#     from pupy.maths import iter_product
+#     ti = time()
+#     iter_product(l)
+#     te = time()
+#     return tictoc.ftime(ti, te)
+#
+#
+# def test_iter_product():
+#     print(_bib_test())
+#
+#
+# test_iter_product()
+#
+# # t = 1
+# # for n in l:
+# #     t *= n
--- setup.py
+++ setup.py
@@ -1,69 +1,28 @@
 # -*- coding: utf-8 -*-
-from distutils.core import setup
 from setuptools import setup, find_packages
 
-# from Cython.Build import cythonize
-# from sys import argv
-#
-# setup(name='pyEuler',
-#       packages=['pupy'],
-#       author='jessekrubin',
-#       author_email='jessekrubin@gmail.com',
-#       description='Pretty_Useful_Python',
-#       requires=['tqdm', 'cython'])
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
+setup(name='pupy',
+      version='2.0.1a',
+      description='pretty useful python',
+      long_description='coming soon',
+      classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+      ],
+      keywords='pretty useful python',
+      # url='http://github.com/jessekrubin/pupy/',
+      url='https://upload.pypi.org/legacy/',
+      author='jesse r',
+      author_email='jessekrubin@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'markdown',
+          'tqdm>=4',
+      ],
+      include_package_data=True,
+      zip_safe=False)
 
-# setup(
-#     name="pupy",
-#     version="1.0.4",
-#     author="jessekrubin",
-#     author_email="jessekrubin@gmail.com",
-#     description="pretty useful python",
-#     long_description="PUP",
-#     long_description_content_type="text/markdown",
-#     url="https://github.com/jessekrubin/pup",
-#     # url='https://upload.pypi.org/legacy/',
-#     packages=['pupy', 'test_pupy'],
-#     classifiers=(
-#         # "Programming Language :: Python :: 3",
-#         # "License :: OSI Approved :: MIT License",
-#         # "Operating System :: OS Independent",
-#     ),
-#     requires=['tqdm'],
-#     python_requires='>=3'
-# )
-from setuptools import setup, find_packages
-setup(
-    name="pupy",
-    version="1.0.6",
-    packages=find_packages('pupy'),
-    # scripts=['pu.py'],
-
-    # Project uses reStructuredText, so ensure that the docutils get
-    # installed or upgraded on the target machine
-    install_requires=['tqdm>=4'],
-
-    package_data={
-        # If any package contains *.txt or *.rst files, include them:
-        # '': ['*.txt', '*.rst'],
-        # And include any *.msg files found in the 'hello' package, too:
-        # 'hello': ['*.msg'],
-    },
-
-    # metadata for upload to PyPI
-    author="jessekrubin",
-    author_email="jessekrubin@gmail.com",
-    description="This is a package",
-    license="MIT",
-    keywords="pretty useful python",
-    url="http://github.com/jessekrubin/pupy/",   # project home page, if any
-    project_urls={
-        # "Documentation": "https://github.com/HelloWorld/",
-        # "Source Code": "https://github.example.com/HelloWorld/",
-    }
-
-    # could also include long_description, download_url, classifiers, etc.
-)
 
