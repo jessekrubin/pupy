1aa1683a7efbf0654e91fca1fb00e6df65ae0e65
Jesse <jessekrubin@gmail.com>
2018-07-30 18:48:09 -0700

    a commmmmit

--- pupy/maths.py
+++ pupy/maths.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # JESSE RUBIN - Biblioteca
+from __future__ import division
 from collections import Counter
 from math import pi, sqrt, acos, factorial
 from operator import floordiv, methodcaller, truediv, add, sub
