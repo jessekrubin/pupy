56229c3faecf8d28e464b5c5119fbcd2cd8347b0
jessekrubin <jessekrubin@gmail.com>
2018-11-30 01:43:00 -0800

    herm

--- setup.py
+++ setup.py
@@ -86,13 +86,13 @@ setup(
     author='jesse k rubin',
     author_email='jessekrubin@gmail.com',
     url='https://github.com/jessekrubin/python-pupy',
+    repository='https://github.com/jessekrubin/python-pupy',
     packages=find_packages('src'),
     package_dir={'': 'src'},
     py_modules=[splitext(basename(path))[0] for path in glob('src/*.py')],
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
@@ -102,24 +102,20 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy',
-        # uncomment if you test on these interpreters:
-        # 'Programming Language :: Python :: Implementation :: IronPython',
-        # 'Programming Language :: Python :: Implementation :: Jython',
-        # 'Programming Language :: Python :: Implementation :: Stackless',
         'Topic :: Utilities',
     ],
     keywords=[
+        'pretty',
+        'useful',
+        'tewls'
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
-    install_requires=[
-        # eg: 'aspectlib==1.1.1', 'six>=1.7',
-    ],
+    install_requires=[],
     extras_require={
         # eg:
         #   'rst': ['docutils>=0.11'],
