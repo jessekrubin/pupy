c9341bf2e898b647f071fb008511c83b98b15c82
jessekrubin <jessekrubin@gmail.com>
2019-02-22 18:54:18 -0800

    herm

--- src/pupy/_version.py
+++ src/pupy/_version.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
-from toml import loads
 from pathlib import Path
 
-def get_version():
-    path = Path(__file__).resolve().parents[2] / 'pyproject.toml'
-    pyproject = loads(open(str(path)).read())
-    return pyproject['tool']['poetry']['version']
-
-__version__ = get_version()
+__version__ = (
+    [
+        l
+        for l in open(str(Path(__file__).resolve().parents[2] / "pyproject.toml"))
+        .read()
+        .split("\n")
+        if "version" in l
+    ][0]
+    .replace("version = ", "")
+    .strip('"')
+)
