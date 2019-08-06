8353d41d891fa0388e374a69ac04a30056f08e89
jesse <jessekrubin@gmail.com>
2019-08-06 10:29:17 -0700

    pushing up

--- pupy/cli.py
+++ pupy/cli.py
@@ -5,11 +5,12 @@
 Pupy CLI
 ========
 """
-from argparse import ZERO_OR_MORE
-from argparse import ArgumentParser
 from codecs import decode
 from os import makedirs
 from os import path as _path
+
+from argparse import ArgumentParser
+from argparse import ZERO_OR_MORE
 
 from pupy._template import _IO_PKG_CLI
 from pupy._template import _IO_SCRIPT
@@ -19,6 +20,10 @@ from pupy._template import _PKG_UTILS
 from pupy._version import __version__
 from pupy.savings_n_loads import sstring
 from pupy.sh import pwd
+
+def base_cmd(args):
+    if args.version:
+        print("pupy version: {}".format(__version__))
 
 def unescaped_str(arg_str):
     """
@@ -35,19 +40,19 @@ def new_package(relative_path):
         sstring(
             _path.join(relative_path, "__init__.py"),
             _PKG_INIT.strip("\n").strip("\r\n") + "\n",
-        )
+            )
         sstring(
             _path.join(relative_path, "__main__.py"),
             _PKG_MAIN.strip("\n").strip("\r\n") + "\n",
-        )
+            )
         sstring(
             _path.join(relative_path, "cli.py"),
             _IO_PKG_CLI.strip("\n").strip("\r\n") + "\n",
-        )
+            )
         sstring(
             _path.join(relative_path, "utils.py"),
             _PKG_UTILS.strip("\n").strip("\r\n") + "\n",
-        )
+            )
         print("Created package: {}".format(relative_path))
     except:
         raise ValueError("{} already exists".format(relative_path))
@@ -70,15 +75,16 @@ PARSER = ArgumentParser(description="Command description.")
 SUBPARSERS = PARSER.add_subparsers(help="commands")
 PARSER.add_argument(
     "-V", "--version", action="store_true", help="Print pupy version."
-)
+    )
+PARSER.set_defaults(func=base_cmd)
 
 NEW_SUBPARSER = SUBPARSERS.add_parser("new")
 NEW_SUBPARSER.add_argument(
     "path", type=unescaped_str, nargs=ZERO_OR_MORE, help="relative path"
-)
+    )
 NEW_SUBPARSER.add_argument(
     "-p", "--package", action="store_true", default=False, help="new package"
-)
+    )
 NEW_SUBPARSER.set_defaults(func=new_cmd)
 
 def main(ARGS=None):
@@ -88,7 +94,5 @@ def main(ARGS=None):
     """
 
     ARGS = PARSER.parse_args(args=ARGS)
-    if ARGS.version:
-        print("pupy version: {}".format(__version__))
     if ARGS.func:
         ARGS.func(ARGS)
